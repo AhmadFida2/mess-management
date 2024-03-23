8d73dd36369fa5b38210378af60c3c92fbf3f566
arsal381 <111340503+arsal381@users.noreply.github.com>
2024-03-23 16:25:10 +0500

    Filament V3 Code Fixes

--- app/Listeners/BillGeneratedListener.php
+++ app/Listeners/BillGeneratedListener.php
@@ -31,14 +31,14 @@ class BillGeneratedListener implements ShouldQueue
      */
     public function handle(BillGenerated $billGenerated)
     {
-        $url = 'https://seeds-erp.app/bill/print/'. $billGenerated->bill->id;
-        $name = $billGenerated->bill->member->name;
-        $email = $billGenerated->bill->member->email;
-        $billAmount = $billGenerated->bill->amount;
-        $units = $billGenerated->bill->units;
-        $dueDate = Carbon::parse($billGenerated->bill->month)->addDays(6)->format('d-M-Y');
-        Mail::to($email)
-            ->send(new BillDetail($name,$billAmount,$units,$dueDate,$url));
+//        $url = 'https://seeds-erp.app/bill/print/'. $billGenerated->bill->id;
+//        $name = $billGenerated->bill->member->name;
+//        $email = $billGenerated->bill->member->email;
+//        $billAmount = $billGenerated->bill->amount;
+//        $units = $billGenerated->bill->units;
+//        $dueDate = Carbon::parse($billGenerated->bill->month)->addDays(6)->format('d-M-Y');
+//        Mail::to($email)
+//            ->send(new BillDetail($name,$billAmount,$units,$dueDate,$url));
     }
 }
 
